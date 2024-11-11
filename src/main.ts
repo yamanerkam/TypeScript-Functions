032b435d19b3095bd61f4d545468f08bdc50fd92
yamanerkam <erkamyaman35@gmail.com>
2024-11-11 19:28:12 +0300

    basic-function

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+function sum(num1:number, num2:number){
+    return num1 + num2;
+}
+// i do not have to write the return type it does it manually
+const sumNums = sum(4,5);
+console.log(sumNums)
+
+// but if i want to it
+
+function multiply(num1:number, num2:number) : number{
+    return num1 + num2;
+}
+
+const multiplyNums = multiply(7,3);
